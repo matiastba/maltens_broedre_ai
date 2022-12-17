2f797a9b3e8b83bbafdaa580793eee97685954ff
matias-cphatomics <72007269+matias-cphatomics@users.noreply.github.com>
2022-12-17 17:54:04 +0100

    restructuring

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,8 +1,7 @@
-from turtledemo.chaos import plot
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 def read_csv(filename):
     data_frame = pd.read_csv(filename, sep=';', encoding='unicode_escape', error_bad_lines=False)
